bbe37087ef182c51b72f5e1650b94e74c9a1d0c4
Chanwoo1124 <lgw22774@gmail.com>
2025-11-30 23:30:28 +0900

    SpringMVC

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
@@ -21,6 +20,7 @@ public class OrderServiceImpl implements OrderService{
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
