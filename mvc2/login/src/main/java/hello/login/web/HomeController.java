0259e5015ead7f8f5e34ceccaf4be2009c3fe0bc
Chanwoo1124 <lgw22774@gmail.com>
2025-12-28 20:57:40 +0900

    filter,interceptor

--- mvc2/login/src/main/java/hello/login/web/HomeController.java
+++ mvc2/login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.session.SessionManger;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,8 +29,7 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String homeLoginV2Spring(
-            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model){
+    public String homeLoginV3ArgumentResolver(@Login Member loginMember, Model model){
 
         //세션에 회원 데이터가 없으면 home
         if (loginMember == null){
