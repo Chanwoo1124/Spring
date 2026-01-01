e6b16542d0881c29e80ac97f703c485c8ef04902
Chanwoo1124 <lgw22774@gmail.com>
2026-01-01 19:05:47 +0900

    exception

---
+++ mvc2/typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,24 @@
+package hello.typeconverter.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request){
+        String data = request.getParameter("data");
+        Integer i = Integer.valueOf(data);
+        System.out.println(i);
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data){
+        System.out.println(data);
+        return "ok";
+    }
+}
