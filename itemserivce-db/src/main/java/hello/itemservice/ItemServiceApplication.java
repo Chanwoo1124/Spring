f4550fd0643782fc363933cce2b4736689b649e9
Chanwoo1124 <lgw22774@gmail.com>
2026-01-11 23:03:37 +0900

    jpa, Spring jpa, mybatis

--- itemserivce-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemserivce-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -13,7 +13,8 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
 @Slf4j
-@Import(JdbcTemplateV3Config.class)
+//@Import(JdbcTemplateV3Config.class)
+@Import(SpringDataJpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
--- itemserivce-db/src/main/java/hello/itemservice/domain/Item.java
+++ itemserivce-db/src/main/java/hello/itemservice/domain/Item.java
@@ -2,15 +2,22 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
-@Data
-public class Item {
+import javax.persistence.*;
 
+@Data
+@Entity
+@Table(name = "item")
+public class Item {
+    //pk 알려줌   //pk 키 어떻게 생성하는지 알려줌
+    @Id         @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name" , length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
 
+    //JPA 사용시 기본생성자 필수
     public Item() {
     }
 
